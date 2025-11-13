0e67217518539e32524108562106625d0af8e7aa
Randy Eckman <randy.eckman@nasa.gov>
2025-11-13 16:40:13 -0600

    more robust timezone handling

--- pycountdown/lib/timezone.py
+++ pycountdown/lib/timezone.py
@@ -4,6 +4,10 @@ HOUR = timedelta(hours=1)
 ZERO = timedelta()
 
 
+class AmbiguousDstError(ValueError):
+    pass
+
+
 class USTimeZone(tzinfo):
     def __init__(self, name: str, std_offset_hr: int):
         self.std_offset = timedelta(hours=-abs(std_offset_hr))
@@ -13,20 +17,65 @@ class USTimeZone(tzinfo):
         self.std_name = f'{basechar}ST'
         self.dst_name = f'{basechar}DT'
 
-    def tzname(self, dt: datetime):
-        return self.dst_name if self.is_dt_dst(dt) else self.std_name
+    def tzname(self, dt: datetime, *, is_utc: bool = None,
+               dst_known: bool = False):
+        return (self.dst_name
+                if self.is_dt_dst(dt, is_utc=is_utc, dst_known=dst_known)
+                else self.std_name)
 
-    def utcoffset(self, dt: datetime):
-        return self.std_offset + self.dst(dt)
+    def utcoffset(self, dt: datetime, *, is_utc: bool = None,
+                  dst_known: bool = False):
+        return self.std_offset + self.dst(dt, is_utc=is_utc,
+                                          dst_known=dst_known)
 
-    def dst(self, dt: datetime):
-        return HOUR if self.is_dt_dst(dt) else ZERO
+    def dst(self, dt: datetime, *, is_utc: bool = None,
+            dst_known: bool = False):
+        return (HOUR if self.is_dt_dst(dt, is_utc=is_utc, dst_known=dst_known)
+                else ZERO)
 
-    def is_dt_dst(self, dt: datetime):
-        # dt is assumed to be the local time, NOT UTC.
+    def is_dt_dst(self, dt: datetime, *, is_utc: bool = None,
+                  dst_known: bool = False):
+        """
+        dst_known should be set to True if we know that dt.fold is
+        correctly set
+        """
         naive_dt = dt.replace(tzinfo=None)
 
-        year = dt.year
+        if is_utc is None:
+            tzi = dt.tzinfo
+            if tzi:
+                if isinstance(tzi, self.__class__):
+                    is_utc = False
+
+                else:
+                    offset = tzi.utcoffset(dt).total_seconds()
+                    is_utc = offset == 0
+
+            else:
+                # assume local time:
+                is_utc = False
+
+        dst_start_utc, dst_end_utc = self.get_utc_dst_start_end(dt.year)
+        if not is_utc:
+            # try to convert to utc, assuming std time
+            naive_dt -= self.std_offset
+            # fold is 1 if we are in std time and know it, so only check if
+            # fold is 0 or not set
+            if not dt.fold:
+                dst_end_ambig_utc = dst_end_utc + HOUR
+                in_ambig = (naive_dt >= dst_end_utc
+                            and naive_dt < dst_end_ambig_utc)
+                if in_ambig:
+                    if dst_known:
+                        naive_dt -= HOUR
+
+                    else:
+                        raise AmbiguousDstError
+
+        # naive_dt is now utc
+        return naive_dt >= dst_start_utc and naive_dt < dst_end_utc
+
+    def get_utc_dst_start_end(self, year: int):
         dst_start_base = datetime(year, 3, 8, 2)
         dst_end_base = datetime(year, 11, 1, 1)
 
@@ -34,11 +83,20 @@ class USTimeZone(tzinfo):
         dst_start_delta = timedelta(days=7 - dst_start_base.isoweekday())
         dst_end_delta = timedelta(days=7 - dst_end_base.isoweekday())
 
-        dst_start = dst_start_base + dst_start_delta
-        dst_end = dst_end_base + dst_end_delta
+        dst_start_std = dst_start_base + dst_start_delta
+        dst_end_std = dst_end_base + dst_end_delta
 
-        # # weekday: 1 = sun, 7 = sat
-        # =AND(G11 >= DATE(YEAR(G11), 3,8+MOD(7-WEEKDAY(DATE(YEAR(G11),3,8),1)+1,7)) + 2/24,  # noqa
-        #      G11 <  DATE(YEAR(G11),11,1+MOD(7-WEEKDAY(DATE(YEAR(G11),11,1),1)+1,7)) + 1/24)  # noqa
+        baseoffset = self.std_offset
+        dst_start_utc = dst_start_std - baseoffset
+        dst_end_utc = dst_end_std - baseoffset
 
-        return naive_dt >= dst_start and naive_dt <= dst_end
+        return dst_start_utc, dst_end_utc
+
+    def fromutc(self, dt: datetime):
+        naive_dt = dt.replace(tzinfo=None)
+        new_dt = dt + self.utcoffset(dt, is_utc=True)
+        _, dst_end_utc = self.get_utc_dst_start_end(dt.year)
+        if naive_dt >= dst_end_utc and naive_dt < (dst_end_utc + HOUR):
+            new_dt = new_dt.replace(fold=1)
+
+        return new_dt
--- tests/pycountdown_test.py
+++ tests/pycountdown_test.py
@@ -2,6 +2,7 @@ from unittest import TestCase, main as utmain, TextTestRunner, mock
 import sys
 from os import environ
 from pathlib import Path
+from datetime import datetime, timedelta
 
 HERE = Path(__file__).expanduser().resolve().parent
 REPOROOT = HERE.parent
@@ -15,11 +16,61 @@ from _pycountdown_testing import (  # noqa: E402
     ENV_PYCOUNTDOWN_UNITTEST_ACTIVE,
 )
 
+ZERO = timedelta()
+HOUR = timedelta(hours=1)
+
 
 @mock.patch.dict(environ, {ENV_PYCOUNTDOWN_UNITTEST_ACTIVE: '1'})
 class TestPyCountdown(TestCase):
     def test_import(self):
         import pycountdown  # noqa: F401
+
+    def single_tz_test(self, local_naive: datetime,
+                       expected_utcoffset_hr: int, expect_error: bool = False,
+                       fold: int = 0):
+        from pycountdown.lib.timeconsts import TZCEN, TZUTC
+        from pycountdown.lib.timezone import AmbiguousDstError
+        local_aware = local_naive.replace(tzinfo=TZCEN, fold=fold)
+        try:
+            local_aware_offset = local_aware.utcoffset()
+        except AmbiguousDstError:
+            if expect_error:
+                local_aware_offset = TZCEN.utcoffset(local_aware,
+                                                     dst_known=True)
+
+            else:
+                self.fail("Unexpected AmbiguousDstError")
+
+        finally:
+            self.assertEqual(local_aware_offset.total_seconds()/3600,
+                             expected_utcoffset_hr)
+
+        utc_naive = local_naive - timedelta(hours=expected_utcoffset_hr)
+        utc_aware = utc_naive.replace(tzinfo=TZUTC)
+        local_fromutc = utc_aware.astimezone(TZCEN)
+        self.assertEqual(local_aware, local_fromutc)
+        self.assertEqual(local_fromutc.fold, fold)
+
+    def test_dst(self):
+        cen_pre_dst_start = datetime(2025, 3, 9, 1)
+        cen_dst_start = datetime(2025, 3, 9, 3)
+        cen_dst_post_start = datetime(2025, 3, 9, 4)
+        cen_pre_dst_end = datetime(2025, 11, 2, 0)
+        cen_dst_end_ambig = datetime(2025, 11, 2, 1)
+        cen_dst_end_midambig = datetime(2025, 11, 2, 1, 59)
+        cen_dst_end = datetime(2025, 11, 2, 2)
+        cen_dst_post_end = datetime(2025, 11, 2, 3)
+
+        self.single_tz_test(cen_pre_dst_start, -6)
+        self.single_tz_test(cen_dst_start, -5)
+        self.single_tz_test(cen_dst_post_start, -5)
+        self.single_tz_test(cen_pre_dst_end, -5)
+        self.single_tz_test(cen_dst_end_ambig, -5, True)
+        self.single_tz_test(cen_dst_end_midambig, -5, True)
+        self.single_tz_test(cen_dst_end_ambig, -6, fold=1)
+        self.single_tz_test(cen_dst_end_midambig, -6, fold=1)
+        self.single_tz_test(cen_dst_end, -6)
+        self.single_tz_test(cen_dst_post_end, -6)
 
 
 if __name__ == '__main__':
